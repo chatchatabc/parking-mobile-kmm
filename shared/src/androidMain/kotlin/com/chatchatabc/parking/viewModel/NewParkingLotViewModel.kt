232051f675ca4819c2c37004b1b58279c19f545e
Raphile14 <rllddalangin@gmail.com>
2023-06-01 17:08:27 +0800

    refactor: modified some variables

--- shared/src/androidMain/kotlin/com/chatchatabc/parking/viewModel/NewParkingLotViewModel.kt
+++ shared/src/androidMain/kotlin/com/chatchatabc/parking/viewModel/NewParkingLotViewModel.kt
@@ -15,8 +15,7 @@ import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 class NewParkingLotViewModel(val api: ParkingAPI, val application: Application): ViewModel() {
-    val popupOpened = MutableStateFlow(false)
-    var currentPage = MutableStateFlow(0)
+    var page = MutableStateFlow(0)
 
     var errors = MutableStateFlow(mapOf<String, String>())
 
@@ -180,8 +179,8 @@ class NewParkingLotViewModel(val api: ParkingAPI, val application: Application):
     fun setToPending() {
         viewModelScope.launch {
             val result = api.setToPending()
-            if (!result.errors.isNullOrEmpty()) {
-                currentPage.value = 3
+            if (result.errors.isEmpty()) {
+                page.value = 3
             }
         }
     }
@@ -200,8 +199,8 @@ class NewParkingLotViewModel(val api: ParkingAPI, val application: Application):
         )
     }
 
-    fun validateCurrentPage(): Boolean {
-        validations[currentPage.value]?.invoke()
+    fun validate(page: Int): Boolean {
+        validations[page]?.invoke()
         return errors.value.isEmpty()
     }
 
@@ -209,7 +208,7 @@ class NewParkingLotViewModel(val api: ParkingAPI, val application: Application):
         validations.onEach { (index, function) ->
             function.invoke()
             if (errors.value.isNotEmpty()) {
-                currentPage.value = index
+                page.value = index
                 return
             }
         }
