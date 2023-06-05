7bd91b8850e42d9bff8346bc9905b8b203555636
Raphile14 <rllddalangin@gmail.com>
2023-06-05 14:36:28 +0800

    chore(client): removed unused imports

--- Parking_Client/src/main/kotlin/com/chatchatabc/parkingclient/android/compose/vehicle/SelectVehicleSheetComposable.kt
+++ Parking_Client/src/main/kotlin/com/chatchatabc/parkingclient/android/compose/vehicle/SelectVehicleSheetComposable.kt
@@ -23,7 +23,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.chatchatabc.parking.model.Vehicle
-import com.chatchatabc.parkingclient.android.compose.vehicle.VehicleItem
 
 @OptIn(ExperimentalMaterialApi::class, ExperimentalMaterial3Api::class)
 @Composable
