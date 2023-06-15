466749875d5d5366e39605e374393b980898835c
Mike Arnado <Mike.Arnado@worldfirst.com>
2023-06-15 09:39:56 +0800

    improvement(shared): Updated DashboardStatistics

--- shared/src/commonMain/kotlin/com/chatchatabc/parking/model/DashboardStatistics.kt
+++ shared/src/commonMain/kotlin/com/chatchatabc/parking/model/DashboardStatistics.kt
@@ -8,5 +8,7 @@ data class DashboardStatistics(
     val leavingSoon: Long,
     val occupiedParkingCapacity: Long,
     val traffic: Long,
+    val trafficPercentage: Double,
     val profit: Double,
+    val profitPercentage: Double
 )
