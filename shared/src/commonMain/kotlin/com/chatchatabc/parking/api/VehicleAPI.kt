d5b6f22dfc1499fdfeec576a1542ea264c247592
Raphile14 <rllddalangin@gmail.com>
2023-06-15 15:38:29 +0800

    feat: implemented updateVehicle

--- shared/src/commonMain/kotlin/com/chatchatabc/parking/api/VehicleAPI.kt
+++ shared/src/commonMain/kotlin/com/chatchatabc/parking/api/VehicleAPI.kt
@@ -2,20 +2,30 @@ package com.chatchatabc.parking.api
 
 import com.chatchatabc.parking.model.Vehicle
 import com.chatchatabc.parking.model.dto.NewVehicleDTO
+import com.chatchatabc.parking.model.dto.UpdateVehicleDTO
 import com.chatchatabc.parking.model.pagination.Page
 import com.chatchatabc.parking.model.response.ApiResponse
 import io.ktor.client.HttpClient
 import io.ktor.http.HttpMethod
 
-class VehicleAPI(val client: HttpClient): AbstractAPI(client) {
+class VehicleAPI(val client: HttpClient) : AbstractAPI(client) {
     val ENDPOINT = "/api/vehicle"
 
     suspend fun register(payload: NewVehicleDTO): ApiResponse<Unit> =
         makeRequest(HttpMethod.Post, "$ENDPOINT/register", payload)
 
+    // TODO: Replace with pagination?
     suspend fun getAllVehicles(): ApiResponse<Page<Vehicle>> =
         makeRequest(HttpMethod.Get, "$ENDPOINT/my-vehicles?limit=1000")
 
     suspend fun getVehicle(vehicleUuid: String): ApiResponse<Vehicle> =
         makeRequest(HttpMethod.Get, "$ENDPOINT/$vehicleUuid")
+
+    /**
+     * Update vehicle
+     */
+    suspend fun updateVehicle(vehicleUuid: String, payload: UpdateVehicleDTO): ApiResponse<Unit> =
+        makeRequest(HttpMethod.Put, "$ENDPOINT/update/$vehicleUuid", payload)
+
+    // TODO: Add the add user and remove user endpoints (not yet needed)
 }
