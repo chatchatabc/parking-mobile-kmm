b112f873c7e40de67d4f661ad8859a603693ff19
Raphile14 <rllddalangin@gmail.com>
2023-06-15 17:19:24 +0800

    feat: implemented getLastUpdatedAtOfRoute

--- shared/src/commonMain/kotlin/com/chatchatabc/parking/api/RouteAPI.kt
+++ shared/src/commonMain/kotlin/com/chatchatabc/parking/api/RouteAPI.kt
@@ -6,6 +6,7 @@ import com.chatchatabc.parking.model.pagination.Pagination
 import com.chatchatabc.parking.model.response.ApiResponse
 import io.ktor.client.HttpClient
 import io.ktor.http.HttpMethod
+import kotlinx.datetime.LocalDateTime
 
 class RouteAPI(client: HttpClient) : AbstractAPI(client) {
     val ENDPOINT = "/api/route"
@@ -15,4 +16,13 @@ class RouteAPI(client: HttpClient) : AbstractAPI(client) {
      */
     suspend fun getAllRoutes(pagination: Pagination? = null): ApiResponse<Page<Route>> =
         makeRequest(HttpMethod.Get, ENDPOINT, pagination = pagination)
+
+    /**
+     * Get last updated status at of a route
+     */
+    suspend fun getLastUpdatedAtOfRoute(
+        routeUuid: String,
+        updatedAt: LocalDateTime
+    ): ApiResponse<Route> =
+        makeRequest(HttpMethod.Get, "$ENDPOINT/$routeUuid/$updatedAt")
 }
