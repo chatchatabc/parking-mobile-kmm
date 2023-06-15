3c8ae1f9d161f206cd549a670d7fd188f1ea19d8
Mike Arnado <Mike.Arnado@worldfirst.com>
2023-06-15 11:34:27 +0800

    chore(client): Code cleanup

--- shared/src/androidMain/kotlin/com/chatchatabc/parking/viewModel/NewVehicleViewModel.kt
+++ shared/src/androidMain/kotlin/com/chatchatabc/parking/viewModel/NewVehicleViewModel.kt
@@ -8,7 +8,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 class NewVehicleViewModel(val vehicleApi: VehicleAPI): BaseViewModel(vehicleApi) {
     var page = MutableStateFlow(0)
     var name = MutableStateFlow("")
-    val platenumber = MutableStateFlow("")
+    val plateNumber = MutableStateFlow("")
     var type = MutableStateFlow(VehicleType.NONE)
 
     val errors = MutableStateFlow(mapOf<String, String>())
@@ -19,22 +19,19 @@ class NewVehicleViewModel(val vehicleApi: VehicleAPI): BaseViewModel(vehicleApi)
             if (errors.value.isNotEmpty()) page.value = currentPage
         }
 
-        if (errors.value.isEmpty()) {
-            load {
-                vehicleApi.register(
-                    NewVehicleDTO(
-                        name = name.value,
-                        plateNumber = platenumber.value,
-                        type = type.value.ordinal
-                    )
-                ).let {
-                    if (it.errors.isEmpty()) {
-                        page.value += 1
-                    } else {
-                        errors.value += errors.value + mapOf("global" to "Something went wrong. Please try again.")
-                    }
-                }
-            }
+        if (errors.value.isEmpty()) submit()
+    }
+
+    fun submit() = load {
+        vehicleApi.register(
+            NewVehicleDTO(
+                name = name.value,
+                plateNumber = plateNumber.value,
+                type = type.value.ordinal
+            )
+        ).let {
+            if (it.errors.isEmpty()) page.value += 1
+            else errors.value += errors.value + mapOf("global" to "Something went wrong. Please try again.")
         }
     }
 
@@ -42,15 +39,15 @@ class NewVehicleViewModel(val vehicleApi: VehicleAPI): BaseViewModel(vehicleApi)
         0 to {
             errors.value = mapOf()
             if (name.value.isEmpty()) errors.value = errors.value + mapOf("name" to "Name cannot be empty")
-            if (platenumber.value.isEmpty()) errors.value = errors.value + mapOf("platenumber" to "Plate number cannot be empty")
+            if (plateNumber.value.isEmpty()) errors.value = errors.value + mapOf("platenumber" to "Plate number cannot be empty")
             if (type.value == VehicleType.NONE) errors.value = errors.value + mapOf("type" to "Please select a vehicle type")
             if (type.value == VehicleType.CAR) {
-                if ("^[A-Z]{2}-\\d{5}\$\n".toRegex().matches(platenumber.value) || // Temporary Car
-                    "^[A-Z]{3}-\\d{4}\$\n".toRegex().matches(platenumber.value) // Permanent Car
+                if ("^[A-Z]{2}-\\d{5}\$\n".toRegex().matches(plateNumber.value) || // Temporary Car
+                    "^[A-Z]{3}-\\d{4}\$\n".toRegex().matches(plateNumber.value)    // Permanent Car
                 ) errors.value = errors.value + mapOf("plateNumber" to "Invalid plate number. Please follow the format of XX-XXXXX or XXX-XXXX")
             } else if (type.value == VehicleType.MOTORCYCLE) {
-                if ("^\\d{4}-\\d{7}\$\n".toRegex().matches(platenumber.value) || // Temporary
-                    "^[A-Z]{2}-\\d{5}\$\n".toRegex().matches(platenumber.value) // Permanent
+                if ("^\\d{4}-\\d{7}\$\n".toRegex().matches(plateNumber.value) || // Temporary
+                    "^[A-Z]{2}-\\d{5}\$\n".toRegex().matches(plateNumber.value)  // Permanent
                 ) errors.value = errors.value + mapOf("plateNumber" to "Invalid plate number. Please follow the format of XXXX-XXXXXXX or XX-XXXXX")
             }
         }
