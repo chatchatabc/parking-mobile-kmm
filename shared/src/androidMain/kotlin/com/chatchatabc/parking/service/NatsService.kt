532af67a329d17a81823b6d9a0926c0d5ac82ac3
Mike Arnado <Mike.Arnado@worldfirst.com>
2023-07-07 13:38:21 +0800

    improvement(client): Restructured NatsService

--- shared/src/androidMain/kotlin/com/chatchatabc/parking/service/NatsService.kt
+++ shared/src/androidMain/kotlin/com/chatchatabc/parking/service/NatsService.kt
@@ -1,97 +1,70 @@
 package com.chatchatabc.parking.service
 
+
 import com.chatchatabc.parking.Config.NATS_BASE_URL
 import io.nats.client.Connection
-import io.nats.client.ConnectionListener
-import io.nats.client.Consumer
-import io.nats.client.ErrorListener
+import io.nats.client.Dispatcher
 import io.nats.client.Message
+import io.nats.client.Nats
 import io.nats.client.Options
-import io.nats.client.impl.NatsImpl
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.launch
-
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import kotlinx.coroutines.withContext
+import java.util.concurrent.ConcurrentHashMap
 
 class NatsService() {
-    enum class ConnectionState {
-        DISCONNECTED,
-        CONNECTED
-    }
+    private var connection: Connection? = null
+    private val subscriptions = ConcurrentHashMap<String, Pair<String, Dispatcher>>()
+    private val mutex = Mutex()
 
-    var notificationId: String = "notificationId"
-    var onMessageRecieved: (Message) -> Unit = { println(it) }
-    var state = MutableStateFlow(ConnectionState.DISCONNECTED)
+    suspend fun initialize(): Boolean = withContext(Dispatchers.IO) {
+        mutex.withLock {
+            return@withLock try {
+                val options = Options.Builder()
+                    .server(NATS_BASE_URL)
+                    .build()
 
-    lateinit var connection: Connection
-
-    fun init(onError: (Exception) -> Unit = {}, callback: NatsService.() -> Unit) {
-        Options.Builder()
-            .server(NATS_BASE_URL)
-            .connectionListener { _, events ->
-                when (events) {
-                    ConnectionListener.Events.DISCONNECTED -> {
-                        println("Disconnected from NATS server")
-                        state.value = ConnectionState.DISCONNECTED
-                    }
-
-                    ConnectionListener.Events.RECONNECTED -> {
-                        println("Reconnected to NATS server")
-                        state.value = ConnectionState.CONNECTED
-                    }
-
-                    ConnectionListener.Events.CLOSED -> {
-                        println("Closed connection to NATS server")
-                        state.value = ConnectionState.DISCONNECTED
-                    }
-
-                    ConnectionListener.Events.CONNECTED -> {
-                        println("Connected to NATS server")
-                        state.value = ConnectionState.CONNECTED
-                    }
-                    else -> {
-                        println("Unknown event: $events")
-                    }
-                }
+                connection = Nats.connect(options)
+                true
+            } catch (e: Exception) {
+                e.printStackTrace()
+                false
             }
-            .errorListener(object : ErrorListener {
-                override fun errorOccurred(conn: Connection?, error: String?) {
-                    println("The server notified the client with: $error")
-                    state.value = ConnectionState.DISCONNECTED
-                }
-
-                override fun exceptionOccurred(conn: Connection?, exp: Exception?) {
-                    println("The connection handled an exception: ${exp!!.localizedMessage}")
-                    state.value = ConnectionState.DISCONNECTED
-                }
-
-                override fun slowConsumerDetected(conn: Connection?, consumer: Consumer?) {
-                    println("The server detected a slow consumer: $consumer")
-                    state.value = ConnectionState.DISCONNECTED
-                }
-            }
-            )
-            .build().let {
-                try {
-                    CoroutineScope(Dispatchers.IO).launch {
-                        connection = NatsImpl.createConnection(it, true)
-                        callback(this@NatsService)
-                    }
-                } catch (exception: Exception) {
-                    onError(exception)
-                }
-            }
-    }
-
-    fun listen(notifId: String) {
-        if (state.value == ConnectionState.CONNECTED) {
-            return
         }
+    }
 
-        notificationId = notifId
+    suspend fun subscribeToSubject(subject: String, onMessageReceived: (Message) -> Unit) {
+        mutex.withLock {
+            println("Subscribing to $subject")
+            val dispatcher = connection?.createDispatcher(onMessageReceived)
+            dispatcher?.let {
+                println("Dispatcher not null, contnuing to subscribe to $subject")
+                it.subscribe(subject)
+                subscriptions[subject] = Pair(subject, it)
+            }
+        }
+    }
 
-        connection.createDispatcher(onMessageRecieved).subscribe(notificationId)
-        state.value = ConnectionState.CONNECTED
+
+    suspend fun unsubscribeFromSubject(subject: String) {
+        mutex.withLock {
+            println("Unsubbing to $subject")
+            subscriptions.remove(subject)?.let {
+                it.second.unsubscribe(it.first)
+            }
+        }
+    }
+
+    suspend fun shutdown() = withContext(Dispatchers.IO) {
+        mutex.withLock {
+            subscriptions.values.forEach {
+                it.second.unsubscribe(it.first)
+            }
+            subscriptions.clear()
+            connection?.close()
+            connection = null
+        }
     }
 }
+
